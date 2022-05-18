4cfb1c18c7b9ef9e18ef551760e144144515451c
oLNidfwworld <kuvetov@bk.ru>
2022-05-18 23:31:22 +0300

    ubogoe

--- kkwat-advanced/menuscript.js
+++ kkwat-advanced/menuscript.js
@@ -1,9 +1,9 @@
 const menu = document.getElementById('menu');
-const menu_list = document.getElementById('menu-list');
-menu.addEventListener('click',()=> {
-    if(menu_list.style.display = "none"){
-        menu_list.style.display = "flex";
-    }else{
-        menu_list.style.display = "none";
-    }
-});
+menu.addEventListener('click',change_menu);
+
+
+
+function change_menu(){
+    const menu_list = document.getElementById('menu-list');
+    menu_list.classList.toggle("open");
+}
